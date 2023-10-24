15a3746cfdc5bd63368a2c2928aafd4a89d4fba3
Pravin <pravin_muruhaya@hotmail.com>
2023-10-24 22:26:24 +0800

    Ignores dirs and sorts all other files in Downloads

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
-import sys
 import os
 import re
+import shutil as st
 
 # parse file tree in Downloads folder
-path = "C:/Users/User/Downloads"  # path to Downloads folder
+path = "C:/Users/User/Downloads/"  # path to Downloads folder
 files = os.listdir(path)  # gather list of files in folder
 exts = set()  # set to store uq file exts
+# sort exts into lists
+images = ['png', 'jpeg', 'jpg']
+docs = ['doc', 'docx', 'txt', 'pdf', 'rmd', 'odt', 'epub']
+data = ['csv', 'xlsx', 'json']
+code = ['py', 'ipynb', 'html']
+list_dirs = ['images', 'docs', 'data', 'code', 'misc']  # list of dirs to create
 
-# access file ext in Downloads dir
+# create all dirs in Downloads dir
+for dir in list_dirs:
+    if os.path.exists(path + dir):
+        print(f'{dir}/ dir exists')
+    else:
+        os.mkdir(path + dir)
+
+# get file extensions
 for file in files:
-    ext = re.findall(r'[.](\w+)$', file)
+    if os.path.isdir(path + file):  # skip dirs
+        continue 
+    else:
+        ext = re.findall(r'[.](\w+)$', file)
     
-    # if no extension, skip
+    # sort extension into dirs
+    # if no extension, put in misc
     if len(ext) == 0:
+        st.move(path + file, path + 'misc/' + file)
         continue
-    
-    # gather all extensions
-    exts.add(ext[0].lower())
-
-# make tmp dir
-try:
-    os.mkdir(path + '/tmp')
-except FileExistsError:
-    sys.exit(1)
-    
-print(exts)
-    
+    elif ext[0].lower() in images:
+        st.move(path + file, path + 'images/' + file)
+    elif ext[0].lower() in docs:
+        st.move(path + file, path + 'docs/' + file)
+    elif ext[0].lower() in data:
+        st.move(path + file, path + 'data/' + file)
+    elif ext[0].lower() in code:
+        st.move(path + file, path + 'code/' + file)
+    else:
+        st.move(path + file, path + 'misc/' + file)
